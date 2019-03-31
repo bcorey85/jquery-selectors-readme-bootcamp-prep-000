1ec52db0656c9e457464a15d5c6a437757d90070
brandon corey <bcorey85@gmail.com>
2019-03-31 18:38:09 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,12 +1,2 @@
 // declare your functions here...
 
-
-
-function callBackTest(func, something){
-  var buildQuote = `This is a test ${something}`
-  func(buildQuote)
-}
-
-function quoteBuilder(yep){
-  console.log(yep)
-}
